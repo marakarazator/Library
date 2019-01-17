9a13c82e4a7ab584986c590099a3d1e75155b96c
Maras <lefmarek@gmail.com>
2019-01-17 10:54:45 +0100

    Simple UserService and UserController

---
+++ src/main/java/com/koddokariery/library/user/UserService.java
@@ -0,0 +1,13 @@
+package com.koddokariery.library.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}

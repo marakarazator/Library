3d4c390f016905686d774b310cba388e21507f3d
Maras <lefmarek@gmail.com>
2019-01-24 11:57:12 +0100

    Frontend: ngRoute with $locationProvider

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,1 +1,22 @@
-var app = angular.module("app", ['ngRoute']);
+'use strict'
+var app = angular.module("app", ["ngRoute"])
+.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider
+		.when("/", {
+			templateUrl: "pages/book.html"
+		})
+		.when("/books", {
+			templateUrl: "pages/books.html"
+		})
+		.when("/users", {
+			templateUrl: "pages/users.html"
+		})
+		.when("/rents", {
+			templateUrl: "pages/rents.html",
+		})
+		.when("/contact", {
+			templateUrl: "pages/contact.html"
+		})
+//		.otherwise({ redirectTo: "/" });
+})
